7e0c40483c38b52238bee7c14f2732c655bbe119
sdp <403213485@qq.com>
2019-09-02 18:47:19 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <input v-model="num"/>
+    <counter @count="count" v-for="n in +num" v-bind:key="n"  v-bind:index="n"/>
+    <span>sum:{{sum}}</span>
+  </div>
+</template>
+
+<script>
+import counter from './components/counter.vue'
+export default {
+  name: 'app',
+  components: {
+    counter
+  },
+
+
+
+data(){
+  return{
+    num:0,
+    sum:0
+    };
+  },
+
+
+methods:{
+  count: function (e) {
+    this.sum = this.sum -e[0]+e[1]
+  }
+
+}
+}
+
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/counter.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="counter">
+  <span>{{ index }}</span>
+  <button @click="add">+</button>
+  <span>{{ number }}</span>
+   <button @click="sub">-</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'counter',
+
+
+data(){
+  return{
+    number:0,
+  };
+},
+
+
+
+props:{
+  index: Number
+},
+
+// created:function(){
+//   window.console.log("现在调用created");
+// },
+
+
+methods:{
+  clickPlus:function(){
+    this.number++;
+  },
+  clickMun:function(){
+    this.number--;
+  },
+
+  add: function(){
+      this.$emit('count',[this.number,++this.number])
+  },
+  sub: function(){
+      this.$emit('count',[this.number,--this.number])
+  }  
+  }
+};
+</script>
+
+
+<style scoped>
+h1{
+  font-weight:normal;
+}
+</style>
+
+
+
